7d43127c9654a8a66e7d0dd6f96468e46b7b10da
Russell <miller.time.baby@gmail.com>
2011-02-18 13:41:51 -0800

    haskell quicksort found

---
+++ haskell/QuickSort.hs
@@ -0,0 +1,10 @@
+module QuickSort where
+
+-- http://en.literateprograms.org/Quicksort_(Haskell)
+-- http://en.wikipedia.org/wiki/Quicksort
+
+quickSort [] = []
+quickSort [x] = [x]
+quickSort (pivot:xs) = (quickSort smaller) ++ [pivot] ++ (quickSort larger)
+  where smaller = filter (<= pivot) xs
+                larger  = filter (> pivot) xs
