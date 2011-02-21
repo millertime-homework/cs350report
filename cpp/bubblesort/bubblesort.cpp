ae6490b014a88512d530e85613889391b39f26a8
carrbs <sanabria@cs.pdx.edu>
2011-02-21 11:05:12 -0800

    added file I/O functionality

--- cpp/bubblesort/bubblesort.cpp
+++ cpp/bubblesort/bubblesort.cpp
@@ -1,60 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
 
 void bubbleSort(int array[]);
-int main()
+
+main(int argc, char** argv)
 {
-	int list[] = {8, 3, 5, 6, 1, 2, 4, 9, 7};
-	int n = 0;
-	cout << "unsorted list:\n{";
-	while (list[n] != '\0')
+        if (argc != 2)
 	{
-		cout << list[n] << ", "; 
-		++n;
+                printf("Nope! Like this: ./bubbles n\n");
+                return 0;
+        }
+
+        int userNum = atoi(&argv[1][0]);
+	int array[userNum + 1]; // creates array to hold names
+	int n = 0; 
+
+	ifstream myfile ("../../size20/list999"); //opening the file.
+    
+	if(myfile.is_open()) //if the file is open
+	{
+		while (!myfile.eof()) //while the end of file is NOT reached
+		{
+			myfile >> array[n];
+            		n++;
+        	}
+
+	myfile.close(); //closing the file
+
 	}
-	cout << "}\n";
-	bubbleSort(list);
+	else cout << "Unable to open file"; //if the file is not open output
+
 	n = 0;
-	cout << "sorted list:\n{";
-	while (list[n] != '\0')
-	{
-		cout << list[n] << ", "; 
-		++n;
-	}
-	cout << "}\n";
+        cout << "unsorted list:\n{";
+        while (array[n] != '\0')
+        {
+                cout << array[n] << ", ";
+                ++n;
+        }	
+        cout << "}\n";
+
+        bubbleSort(array);
+        n = 0;
+        cout << "sorted list:\n{";
+        while (array[n] != '\0')
+        {
+                cout << array[n] << ", ";
+                ++n;
+        }
+        cout << "}\n";
 	return 0;
 }
 
 void bubbleSort(int array[])
 {
-	int swap;
-	do{
-		swap = 0;
-		int n = 1;
-		while (array[n] != '\0')
-		{
-			if (array[n-1] > array[n])
-			{
-				swap = 1;
-				int temp = array[n-1];
-				array[n-1] = array[n];
-				array[n] = temp;
-			}
-			++n;
-		}
-	}while(swap == 1);
-}
-
-
-/*  do{
-    swapped = false
-    for each i in 1 to length(A) - 1 inclusive do:
-      if A[i-1] > A[i] then
-        swap( A[i-1], A[i] )
-        swapped = true
-      end if
-    end for
-  while swapped
-end procedure
-*/
+        int swap;
+        do{
+                swap = 0;
+                int n = 1;
+                while (array[n] != '\0')
+                {
+                        if (array[n-1] > array[n])
+                        {
+                                swap = 1;
+                                int temp = array[n-1];
+                                array[n-1] = array[n];
+                                array[n] = temp;
+                        }
+                        ++n;
+                }
+        }while(swap == 1);
+}	
