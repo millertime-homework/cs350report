e63d6594dc16fc8f48081abee951207078919038
carrbs <sanabria@cs.pdx.edu>
2011-02-26 15:36:47 -0800

    adding more file I/O functionality and trying to break git

--- cpp/allSort/allSort.cpp
+++ cpp/allSort/allSort.cpp
@@ -19,27 +19,22 @@ void bubbleSort(int array[]);
 void insertionSort(int arr[], int length);
 
 // Get unsorted lists of integers
-void getList(int array[], int userNumI, char userNum[]);
+void getList(int array[], int sizeInt, char sizeChar[], int listNum);
 
 // Print array contents
 void printList(int array[]);
 
 main(int argc, char** argv)
 {
-    if (argc != 2)
-    {
-        printf("Nope! Like this: ./allSort n\n");
-        return 0;
-    }
+	int sizes[6] = {10,50,100,500,1000,5000}; // the sizes of lists
+	int sizeInt = sizes[4]; 
+	char sizeChar[50];
+    	int array[sizeInt + 1]; // creates array to hold names
+	sprintf (sizeChar, "%d", sizeInt);
+	int listNum = 1; // 1-1000
 	ofstream outFile;
   	outFile.open ("output.txt");
-  	outFile << "BubbleSort, QuickSort, InsertionSort, MergeSort\n";
-  	outFile.close();
-
-    int userNum = atoi(&argv[1][0]);
-    int array[userNum + 1]; // creates array to hold names
-    getList(array, userNum, &argv[1][0]);
-
+  	outFile << "n, BubbleSort, QuickSort, InsertionSort, MergeSort\n";
     /*
        cout << "unsorted list:\n{";
        printList(array);
@@ -49,10 +44,13 @@ main(int argc, char** argv)
     timeval t1;
     timeval t2;
 
-    // Bubble Sort	
-    gettimeofday(&t1, NULL);
-    bubbleSort(array);
-    gettimeofday(&t2, NULL);
+while (listNum <= 1000)
+{
+ 	getList(array, sizeInt, sizeChar, listNum);
+	// Bubble Sort	
+	gettimeofday(&t1, NULL);
+ 	bubbleSort(array);
+	gettimeofday(&t2, NULL);
 
     /*
        cout << "bubbleSorted list:\n{";
@@ -60,21 +58,20 @@ main(int argc, char** argv)
        cout << "}\n";
 
      */
-    //cout << "t1: " << t1.tv_usec << ", t2: " << t2.tv_usec << endl;
-    cout << "bubbleSort execution time on n = "<< userNum << ": " << t2.tv_usec - t1.tv_usec << endl;
-
-    //getList(array, userNum);
-
+    	//cout << "t1: " << t1.tv_usec << ", t2: " << t2.tv_usec << endl;
+//	cout << "bubbleSort execution time on n = "<< sizeInt << ": " << t2.tv_usec - t1.tv_usec << endl;
+ 	outFile << sizeInt << "," << t2.tv_usec - t1.tv_usec << ",";
+	getList(array, sizeInt, sizeChar, listNum);
     /*
-       cout << "unsorted list:\n{";
-       printList(array);
-       cout << "}\n";
+ 	cout << "unsorted list:\n{";
+	printList(array);
+ 	cout << "}\n";
      */
 
     // Quick Sort	
-    gettimeofday(&t1, NULL);
-    quickSort(array, 0, 1000);
-    gettimeofday(&t2, NULL);
+ 	gettimeofday(&t1, NULL);
+ 	quickSort(array, 0, 1000);
+	gettimeofday(&t2, NULL);
 
     /*
        cout << "quickSorted list:\n{";
@@ -83,19 +80,19 @@ main(int argc, char** argv)
 
        cout << "t1: " << t1 << ", t2: " << t2 << endl;
      */
-    cout << "quickSort execution time on n = "<< userNum << ": " << t2.tv_usec - t1.tv_usec << endl;
-
-    //getList(array, userNum);
-
+//	cout << "quickSort execution time on n = "<< sizeInt << ": " << t2.tv_usec - t1.tv_usec << endl;
+	outFile << t2.tv_usec - t1.tv_usec << ",";	
+	
+	getList(array, sizeInt, sizeChar, listNum);
 	/*
 	cout << "unSorted list:\n{";
     	printList(array);
     	cout << "}\n";
 	*/
 
-    gettimeofday(&t1, NULL);
-    insertionSort(array, userNum);
-    gettimeofday(&t2, NULL);
+	gettimeofday(&t1, NULL);
+	insertionSort(array, sizeInt);
+	gettimeofday(&t2, NULL);
 
 	/*
     cout << "insertionSorted list:\n{";
@@ -103,8 +100,12 @@ main(int argc, char** argv)
     cout << "}\n";
 	*/
 
-    cout << "insertionSort execution time on n = "<< userNum << ": " << t2.tv_usec - t1.tv_usec << endl;
+//	cout << "insertionSort execution time on n = "<< sizeInt << ": " << t2.tv_usec - t1.tv_usec << endl;
+	outFile << t2.tv_usec - t1.tv_usec << "\n";
+	++listNum;
+}
 
+    outFile.close();
     return 0;
 }
 
@@ -154,20 +155,21 @@ void quickSort(int a[], int p, int r) {
     }
 }
 
-void getList(int array[], int userNumI, char userNum[])
+void getList(int array[], int sizeInt, char sizeChar[], int listNum)
 {
-    int n = 0;
-
-	char pathName[20]; 
+    	int n = 0;
+	char listChar[50];
+	char pathName[50]; 
 	strcpy (pathName, "../../size");
-	strcat(pathName, userNum);
+	strcat(pathName, sizeChar);
 	strcat(pathName, "/");
-	strcat(pathName, "list998");
-	//char fileName[] = 	
+	strcat(pathName, "list");
+        sprintf (listChar, "%d", listNum);
+	strcat(pathName, listChar);
     ifstream myfile (pathName); //opening the file.
     if(myfile.is_open()) //if the file is open
     {   
-        while (!myfile.eof() && (n < userNumI)) //while the end of file is NOT reached
+        while (!myfile.eof() && (n < sizeInt)) //while the end of file is NOT reached
         {   
             myfile >> array[n];
             n++;
