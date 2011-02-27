2d336721c106f2038b9301d551690876287db6c5
Russell <miller.time.baby@gmail.com>
2011-02-26 16:03:47 -0800

    cleaned up some inconsistent indentation

--- cpp/allSort/allSort.cpp
+++ cpp/allSort/allSort.cpp
@@ -26,15 +26,15 @@ void printList(int array[]);
 
 int main(int argc, char** argv)
 {
-	int sizes[6] = {10,50,100,500,1000,5000}; // the sizes of lists
-	int sizeInt = sizes[4]; 
-	char sizeChar[50];
-    	int array[sizeInt + 1]; // creates array to hold names
-	sprintf (sizeChar, "%d", sizeInt);
-	int listNum = 1; // 1-1000
-	ofstream outFile;
-  	outFile.open ("output.txt");
-  	outFile << "n, BubbleSort, QuickSort, InsertionSort, MergeSort\n";
+    int sizes[6] = {10,50,100,500,1000,5000}; // the sizes of lists
+    int sizeInt = sizes[4];
+    char sizeChar[50];
+        int array[sizeInt + 1]; // creates array to hold names
+    sprintf (sizeChar, "%d", sizeInt);
+    int listNum = 1; // 1-1000
+    ofstream outFile;
+      outFile.open ("output.txt");
+      outFile << "n, BubbleSort, QuickSort, InsertionSort, MergeSort\n";
     /*
        cout << "unsorted list:\n{";
        printList(array);
@@ -44,13 +44,13 @@ int main(int argc, char** argv)
     timeval t1;
     timeval t2;
 
-while (listNum <= 1000)
-{
- 	getList(array, sizeInt, sizeChar, listNum);
-	// Bubble Sort	
-	gettimeofday(&t1, NULL);
- 	bubbleSort(array);
-	gettimeofday(&t2, NULL);
+    while (listNum <= 1000)
+    {
+        getList(array, sizeInt, sizeChar, listNum);
+        // Bubble Sort
+        gettimeofday(&t1, NULL);
+        bubbleSort(array);
+        gettimeofday(&t2, NULL);
 
     /*
        cout << "bubbleSorted list:\n{";
@@ -58,20 +58,20 @@ while (listNum <= 1000)
        cout << "}\n";
 
      */
-    	//cout << "t1: " << t1.tv_usec << ", t2: " << t2.tv_usec << endl;
-//	cout << "bubbleSort execution time on n = "<< sizeInt << ": " << t2.tv_usec - t1.tv_usec << endl;
- 	outFile << sizeInt << "," << t2.tv_usec - t1.tv_usec << ",";
-	getList(array, sizeInt, sizeChar, listNum);
+        //cout << "t1: " << t1.tv_usec << ", t2: " << t2.tv_usec << endl;
+//    cout << "bubbleSort execution time on n = "<< sizeInt << ": " << t2.tv_usec - t1.tv_usec << endl;
+        outFile << sizeInt << "," << t2.tv_usec - t1.tv_usec << ",";
+        getList(array, sizeInt, sizeChar, listNum);
     /*
- 	cout << "unsorted list:\n{";
-	printList(array);
- 	cout << "}\n";
+     cout << "unsorted list:\n{";
+    printList(array);
+     cout << "}\n";
      */
 
-    // Quick Sort	
- 	gettimeofday(&t1, NULL);
- 	quickSort(array, 0, 1000);
-	gettimeofday(&t2, NULL);
+    // Quick Sort    
+        gettimeofday(&t1, NULL);
+        quickSort(array, 0, 1000);
+        gettimeofday(&t2, NULL);
 
     /*
        cout << "quickSorted list:\n{";
@@ -80,31 +80,30 @@ while (listNum <= 1000)
 
        cout << "t1: " << t1 << ", t2: " << t2 << endl;
      */
-//	cout << "quickSort execution time on n = "<< sizeInt << ": " << t2.tv_usec - t1.tv_usec << endl;
-	outFile << t2.tv_usec - t1.tv_usec << ",";	
-	
-	getList(array, sizeInt, sizeChar, listNum);
-	/*
-	cout << "unSorted list:\n{";
-    	printList(array);
-    	cout << "}\n";
-	*/
+//    cout << "quickSort execution time on n = "<< sizeInt << ": " << t2.tv_usec - t1.tv_usec << endl;
+        outFile << t2.tv_usec - t1.tv_usec << ",";
 
-	gettimeofday(&t1, NULL);
-	insertionSort(array, sizeInt);
-	gettimeofday(&t2, NULL);
+        getList(array, sizeInt, sizeChar, listNum);
+    /*
+    cout << "unSorted list:\n{";
+        printList(array);
+        cout << "}\n";
+    */
 
-	/*
+        gettimeofday(&t1, NULL);
+        insertionSort(array, sizeInt);
+        gettimeofday(&t2, NULL);
+
+    /*
     cout << "insertionSorted list:\n{";
     printList(array);
     cout << "}\n";
-	*/
+    */
 
-//	cout << "insertionSort execution time on n = "<< sizeInt << ": " << t2.tv_usec - t1.tv_usec << endl;
-	outFile << t2.tv_usec - t1.tv_usec << "\n";
-	++listNum;
-}
-
+//    cout << "insertionSort execution time on n = "<< sizeInt << ": " << t2.tv_usec - t1.tv_usec << endl;
+        outFile << t2.tv_usec - t1.tv_usec << "\n";
+        ++listNum;
+    }
     outFile.close();
     return 0;
 }
@@ -127,7 +126,7 @@ void bubbleSort(int array[])
             ++n;
         }
     }while(swap == 1);
-}	
+}
 
 int partition(int a[], int p, int r) {
     int x = a[r];
@@ -157,30 +156,30 @@ void quickSort(int a[], int p, int r) {
 
 void getList(int array[], int sizeInt, char sizeChar[], int listNum)
 {
-    	int n = 0;
-	char listChar[50];
-	char pathName[50]; 
-	strcpy (pathName, "../../size");
-	strcat(pathName, sizeChar);
-	strcat(pathName, "/");
-	strcat(pathName, "list");
-        sprintf (listChar, "%d", listNum);
-	strcat(pathName, listChar);
+    int n = 0;
+    char listChar[50];
+    char pathName[50];
+    strcpy (pathName, "../../size");
+    strcat(pathName, sizeChar);
+    strcat(pathName, "/");
+    strcat(pathName, "list");
+    sprintf (listChar, "%d", listNum);
+    strcat(pathName, listChar);
     ifstream myfile (pathName); //opening the file.
     if(myfile.is_open()) //if the file is open
-    {   
+    {
         while (!myfile.eof() && (n < sizeInt)) //while the end of file is NOT reached
-        {   
+        {
             myfile >> array[n];
             n++;
-        }   
+        }
         array[n] = '\0';
         myfile.close(); //closing the file
 
-    }   
+    }
     else {
-      cerr << "Unable to open file\n"; //if the file is not open output
-      exit(0);
+        cerr << "Unable to open file\n"; //if the file is not open output
+        exit(0);
     }
 }
 
@@ -188,28 +187,24 @@ void printList(int array[])
 {
     int n = 0;
     while (array[n] != '\0')
-    {   
+    {
         cout << array[n] << ", ";
         ++n;
-    }   
+    }
 }
 
-void insertionSort(int arr[], int length) 
+void insertionSort(int arr[], int length)
 {
-
     int i, j, tmp;
-    for (i = 1; i < length; i++) 
+    for (i = 1; i < length; i++)
     {
         j = i;
-        while (j > 0 && arr[j - 1] > arr[j]) 
+        while (j > 0 && arr[j - 1] > arr[j])
         {
             tmp = arr[j];
-            arr[j] = arr[j - 1]; 
+            arr[j] = arr[j - 1];
             arr[j - 1] = tmp;
             j--;
-
-        }   
-
-    }   
-
+        }
+    }
 }
